51b1856152bd0f3aaf15804a87a4d8a48a29d941
Alexis Ortega <alexis.oc3@gmail.com>
2023-01-12 00:16:44 -0700

    Initial commit

---
+++ lib/runrun.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "runrun/version"
+
+module Runrun
+  class Error < StandardError; end
+  # Your code goes here...
+  def self.main
+    puts "Hello World!"
+  end
+end
